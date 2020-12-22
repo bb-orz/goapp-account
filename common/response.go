518f2199343c14efb102aaf286f5ff31b4d370d3
gofuncchan <gofuncchan@163.com>
2020-12-23 01:07:58 +0800

    init code ErrorMiddleware and ResponseMiddleware

--- common/response.go
+++ common/response.go
@@ -1,3 +1,11 @@
 package common
 
+import "github.com/gin-gonic/gin"
+
 /* 统一响应信息格式 */
+
+func ResponseMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
--- restful/middleware/error_middleware.go
+++ restful/middleware/error_middleware.go
@@ -2,12 +2,27 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"goinfras-sample-account/common"
 )
 
 func ErrorMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		 if len(ctx.Errors) > 0 {
+		// TODO 判断错误类型是服务端内部错误还是客户端逻辑错误
 
+		 if len(ctx.Errors) > 0 {
+		 	e := ctx.Errors[0].Err
+			switch e.(type) {
+			case common.SError :
+				// TODO 服务端错误记录日志 ...，并返回统一的服务端错误信息，隐藏内部错误
+
+
+
+			case common.CError:
+				// TODO 返回定制的客户端错误信息
+
+
+
+			}
 
 
 		 }
