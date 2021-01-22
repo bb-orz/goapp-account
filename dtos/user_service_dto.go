1ad0d35cc3ebe788677e3e99c4347c7d3ffb6dbf
gofuncchan <gofuncchan@163.com>
2021-01-22 16:00:32 +0800

    fix model dto

--- dtos/user_service_dto.go
+++ dtos/user_service_dto.go
@@ -9,14 +9,14 @@ import "time"
 // 用户数据传输对象
 type UserInfoDTO struct {
 	Id            uint      `validate:"required,numeric" json:"id"`                 // 用户id
-	No            string    `validate:"required,alphanumunicode" json:"no"`         // 用户生成编号
-	Name          string    `validate:"required,alphanumunicode" json:"name"`       // 用户名
+	No            string    `validate:"required,alphanum" json:"no"`                // 用户生成编号
+	Name          string    `validate:"required,alphanum" json:"name"`              // 用户名
 	Age           uint      `validate:"required,numeric,,max=100,min=0" json:"age"` // 用户年龄
 	Gender        uint      `validate:"required,numeric,max=1,min=0" json:"gender"` // 用户性别
 	Avatar        string    `validate:"required,uri" json:"avatar"`                 // 用户头像
-	Email         string    `validate:"required,alphanumunicode" json:"email"`      // 账户邮箱
+	Email         string    `validate:"required,alphanum" json:"email"`             // 账户邮箱
 	EmailVerified int       `validate:"required,numeric" json:"email_verified"`     // 邮箱是否已验证
-	Phone         string    `validate:"required,alphanumunicode" json:"phone"`      // 账户手机号码
+	Phone         string    `validate:"required,alphanum" json:"phone"`             // 账户手机号码
 	PhoneVerified int       `validate:"required,numeric" json:"phone_verified"`     // 手机号码是否已验证
 	Status        int       `validate:"required,numeric" json:"status"`             // 账户状态：1：启用，0：停用
 	CreatedAt     time.Time `validate:"required,numeric" json:"created_at"`         //
@@ -26,14 +26,14 @@ type UserInfoDTO struct {
 
 type UserOAuthInfoDTO struct {
 	Id            uint      `validate:"required,numeric" json:"id"`                 // 用户id
-	No            string    `validate:"required,alphanumunicode" json:"no"`         // 用户生成编号
-	Name          string    `validate:"required,alphanumunicode" json:"name"`       // 用户名
+	No            string    `validate:"required,alphanum" json:"no"`                // 用户生成编号
+	Name          string    `validate:"required,alphanum" json:"name"`              // 用户名
 	Age           uint      `validate:"required,numeric,,max=100,min=0" json:"age"` // 用户年龄
 	Gender        uint      `validate:"required,numeric,max=1,min=0" json:"gender"` // 用户性别
 	Avatar        string    `validate:"required,uri" json:"avatar"`                 // 用户头像
-	Email         string    `validate:"required,alphanumunicode" json:"email"`      // 账户邮箱
+	Email         string    `validate:"required,alphanum" json:"email"`             // 账户邮箱
 	EmailVerified int       `validate:"required,numeric" json:"email_verified"`     // 邮箱是否已验证
-	Phone         string    `validate:"required,alphanumunicode" json:"phone"`      // 账户手机号码
+	Phone         string    `validate:"required,alphanum" json:"phone"`             // 账户手机号码
 	PhoneVerified int       `validate:"required,numeric" json:"phone_verified"`     // 手机号码是否已验证
 	Status        int       `validate:"required,numeric" json:"status"`             // 账户状态：1：启用，0：停用
 	CreatedAt     time.Time `validate:"required,numeric" json:"created_at"`         //
@@ -56,8 +56,8 @@ type IsPhoneAccountExistDTO struct {
 type CreateUserWithEmailDTO struct {
 	Name       string `validate:"required,alphanum,max=16,min=4" json:"name"`
 	Email      string `validate:"required,email" json:"email"`
-	Password   string `validate:"required,alphanumunicode,max=20,min=8" json:"password"`
-	RePassword string `validate:"required,alphanumunicode,eqfield=Password,max=20,min=8" json:"repassword"`
+	Password   string `validate:"required,alphanum,max=20,min=8" json:"password"`
+	RePassword string `validate:"required,alphanum,eqfield=Password,max=20,min=8" json:"repassword"`
 }
 
 // 创建用户的数据传输对象
@@ -65,14 +65,14 @@ type CreateUserWithPhoneDTO struct {
 	Name       string `validate:"required,alphanum,max=16,min=4" json:"name"`
 	Phone      string `validate:"required,numeric,len=11" json:"phone"`
 	VerifyCode string `validate:"required,alphanum,len=6" json:"verify_code"`
-	Password   string `validate:"required,alphanumunicode,max=20,min=8" json:"password"`
-	RePassword string `validate:"required,alphanumunicode,max=20,min=8,eqfield=Password" json:"repassword"`
+	Password   string `validate:"required,alphanum,max=20,min=8" json:"password"`
+	RePassword string `validate:"required,alphanum,max=20,min=8,eqfield=Password" json:"repassword"`
 }
 
 // 邮箱密码鉴权数据传输对象
 type AuthWithEmailPasswordDTO struct {
 	Email    string `validate:"required,email" json:"email"`
-	Password string `validate:"required,alphanumunicode,max=20,min=8" json:"password"`
+	Password string `validate:"required,alphanum,max=20,min=8" json:"password"`
 }
 
 // 手机号密码鉴权数据传输对象
@@ -116,17 +116,17 @@ type PhoneValidateDTO struct {
 // 更改密码数据传输对象
 type ModifiedPasswordDTO struct {
 	Id    uint   `validate:"required,numeric" json:"id"`
-	Old   string `validate:"required,alphanumunicode,max=20,min=8" json:"old"`
-	New   string `validate:"required,alphanumunicode,max=20,min=8" json:"new"`
-	ReNew string `validate:"required,alphanumunicode,max=20,min=8,eqfield=New" json:"renew"`
+	Old   string `validate:"required,alphanum,max=20,min=8" json:"old"`
+	New   string `validate:"required,alphanum,max=20,min=8" json:"new"`
+	ReNew string `validate:"required,alphanum,max=20,min=8,eqfield=New" json:"renew"`
 }
 
 // 忘记密码重设数据传输对象
 type ResetForgetPasswordDTO struct {
 	Email      string `validate:"required,email" json:"email"`
 	VerifyCode string `validate:"required,alphanum,len=6" json:"code"` // 允许重设密码的key值，服务端生成后被发往邮箱，用户点击过来后接收
-	New        string `validate:"required,alphanumunicode,max=20,min=8" json:"new"`
-	ReNew      string `validate:"required,alphanumunicode,max=20,min=8,eqfield=New" json:"renew"`
+	New        string `validate:"required,alphanum,max=20,min=8" json:"new"`
+	ReNew      string `validate:"required,alphanum,max=20,min=8,eqfield=New" json:"renew"`
 }
 
 type SetAvatarUriDTO struct {
--- dtos/users_dto.go
+++ dtos/users_dto.go
@@ -7,22 +7,22 @@ import (
 
 // UsersDTO is a mapping object for users table in mysql
 type UsersDTO struct {
-	Id            uint      `validate:"required,numeric" json:"id"`               // 用户id
-	No            string    `validate:"required,alphanumunicode" json:"no"`       // 用户生成编号
-	Name          string    `validate:"required,alphanumunicode" json:"name"`     // 用户名
-	Age           uint      `validate:"required,numeric" json:"age"`              // 用户年龄
-	Gender        uint      `validate:"required,numeric" json:"gender"`           // 用户性别
-	Avatar        string    `validate:"required,alphanumunicode" json:"avatar"`   // 用户头像
-	Email         string    `validate:"required,alphanumunicode" json:"email"`    // 账户邮箱
-	EmailVerified int       `validate:"required,numeric" json:"email_verified"`   // 邮箱是否已验证
-	Phone         string    `validate:"required,alphanumunicode" json:"phone"`    // 账户手机号码
-	PhoneVerified int       `validate:"required,numeric" json:"phone_verified"`   // 手机号码是否已验证
-	Password      string    `validate:"required,alphanumunicode" json:"password"` // 用户已加密密码字符串
-	Salt          string    `validate:"required,alphanumunicode" json:"salt"`     // 加密盐
-	Status        int       `validate:"required,numeric" json:"status"`           // 账户状态：1：启用，0：停用
-	CreatedAt     time.Time `validate:"required,numeric" json:"created_at"`       //
-	UpdatedAt     time.Time `validate:"required,numeric" json:"updated_at"`       //
-	DeletedAt     time.Time `validate:"required,numeric" json:"deleted_at"`       //
+	Id            uint      `validate:"numeric" json:"id"`                  // 用户id
+	No            string    `validate:"alphanum" json:"no"`                 // 用户生成编号
+	Name          string    `validate:"alphanum" json:"name"`               // 用户名
+	Age           uint      `validate:"numeric" json:"age"`                 // 用户年龄
+	Gender        uint      `validate:"numeric" json:"gender"`              // 用户性别
+	Avatar        string    `validate:"alphanum" json:"avatar"`             // 用户头像
+	Email         string    `validate:"alphanum" json:"email"`              // 账户邮箱
+	EmailVerified int       `validate:"numeric" json:"email_verified"`      // 邮箱是否已验证
+	Phone         string    `validate:"alphanum" json:"phone"`              // 账户手机号码
+	PhoneVerified int       `validate:"numeric" json:"phone_verified"`      // 手机号码是否已验证
+	Password      string    `validate:"alphanum" json:"password"`           // 用户已加密密码字符串
+	Salt          string    `validate:"alphanum" json:"salt"`               // 加密盐
+	Status        int       `validate:"numeric" json:"status"`              // 账户状态：1：启用，0：停用
+	CreatedAt     time.Time `validate:"numeric" json:"created_at"`          //
+	UpdatedAt     time.Time `validate:"numeric" json:"updated_at"`          //
+	DeletedAt     time.Time `validate:"required,numeric" json:"deleted_at"` //
 }
 
 func (dto *UsersDTO) Validate() error {
